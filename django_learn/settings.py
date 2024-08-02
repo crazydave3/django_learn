99cf4a533daea4fa44b0b93a24b083d100bffa65
hjm <912059894@qq.com>
2024-08-02 17:23:55 +0800

    use postgresql data bases

--- django_learn/settings.py
+++ django_learn/settings.py
@@ -74,12 +74,23 @@ WSGI_APPLICATION = 'django_learn.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'polls',
+        "USER": 'postgres',
+        'PASSWORD': '123456',
+        "HOST": 'localhost',
+        'PORT': '5432'
     }
 }
+
 
 
 # Password validation
